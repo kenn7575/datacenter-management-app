d7e46f67faaaef7a3a42824b467a848ce215858c
kenni kollemorten <kenn7575@gmail.com>
2025-02-07 09:41:51 +0100

    feat: add QR scanner and loan details pages, update dependencies and iOS configurations

--- app/lib/src/app_router.dart
+++ app/lib/src/app_router.dart
@@ -1,31 +1,53 @@
+import 'package:app/src/LoanDetails/loan_details_page.dart';
+import 'package:app/src/main_route_layout.dart';
+import 'package:app/src/scanner/qr_scanner_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'login/auth_provider.dart';
 import 'login/login_page.dart';
-// import '../screens/home_page.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: "/login",
-    refreshListenable: AuthProvider(), // Listens for auth state changes
     redirect: (context, state) {
-      final authProvider = Provider.of<AuthProvider>(context, listen: false);
+      final authProvider = Provider.of<AuthProvider>(context, listen: true);
       final isAuthenticated = authProvider.isAuthenticated;
-      
-      if (!isAuthenticated && state.path != '/login') {
+
+      if (!isAuthenticated && state.fullPath != '/login') {
         return '/login';
       }
-      if (isAuthenticated && state.path == '/login') {
+      if (isAuthenticated && state.fullPath == '/login') {
         return '/';
       }
       return null; // No redirection
     },
     routes: [
-      // GoRoute(
-      //   path: '/',
-      //   builder: (context, state) => HomePage(),
-      // ),
+      ShellRoute(
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (context, state) => QrScannerPage(),
+          ),
+          GoRoute(
+            path: '/scanner',
+            builder: (context, state) => Text("Scanner"),
+          ),
+          GoRoute(
+              path: "/scanner/:loanId",
+              builder: (context, state) {
+                //TODO: Handle null
+                final loanId = state.pathParameters['loanId']!;
+                return LoanDetailsPage(
+                  loanId: loanId,
+                );
+              }),
+        ],
+        builder: (context, state, child) {
+          return MainRouteLayout(
+            child: child,
+          );
+        },
+      ),
       GoRoute(
         path: '/login',
         builder: (context, state) => LoginPage(),
---
+++ app/lib/src/main_route_layout.dart
@@ -0,0 +1,43 @@
+import 'package:app/src/login/auth_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MainRouteLayout extends StatefulWidget {
+  const MainRouteLayout({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  State<MainRouteLayout> createState() => _MainRouteLayoutState();
+}
+
+class _MainRouteLayoutState extends State<MainRouteLayout> {
+  Widget get child => widget.child;
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context, listen: true);
+
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code),
+            label: 'Scan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'User',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 1) {
+            print("Logout");
+            authProvider.logout();
+          }
+        },
+      ),
+      body: child,
+    );
+  }
+}
